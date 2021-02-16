fbedc64533e8da10f36b42a8912c6cb3f8af3bb7
Ravil Galeyev <dehasi@yandex.ru>
2021-02-16 23:01:38 +0100

    Add collector

--- src/main/java/mystream/Potok.java
+++ src/main/java/mystream/Potok.java
@@ -2,28 +2,43 @@ package mystream;
 
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collector;
+import java.util.stream.Stream;
 
-class Potok<TYPE> {
+class Potok<IN, OUT> {
 
-    private final Spliterator<TYPE> spliterator;
+    private final Spliterator<IN> spliterator;
+    private final Function<IN, OUT> transform;
 
-    private Potok(Spliterator<TYPE> spliterator) {
+//    private Potok(Spliterator<IN> spliterator) {
+//        this(spliterator, x -> x);
+//    }
+
+    private Potok(Spliterator<IN> spliterator, Function<IN, OUT> transform) {
         this.spliterator = spliterator;
+        this.transform = transform;
     }
 
     @SafeVarargs
-    static <TYPE> Potok<TYPE> of(TYPE... elements) {
-        return new Potok<>(new MyArraySpliterator<>(elements));
+    static <TYPE> Potok<TYPE, TYPE> of(TYPE... elements) {
+        return new Potok<>(new MyArraySpliterator<>(elements),  x -> x);
     }
 
-    void forEach(Consumer<? super TYPE> consumer) {
+
+//    <RESULT> Stream<RESULT> map(Function<? super IN, ? extends RESULT> mapper) {
+//
+//        return new Potok<IN, RESULT>(spliterator, transform.andThen(mapper););
+//    }
+
+
+    void forEach(Consumer<? super IN> consumer) {
         while (spliterator.tryAdvance(consumer)) ;
     }
 
-    <RESULT, ACCUMULATOR> RESULT collect(Collector<? super TYPE, ACCUMULATOR, RESULT> collector) {
+    <RESULT, ACCUMULATOR> RESULT collect(Collector<? super OUT, ACCUMULATOR, RESULT> collector) {
         ACCUMULATOR accumulator = collector.supplier().get();
-        spliterator.forEachRemaining(element -> collector.accumulator().accept(accumulator, element));
+        spliterator.forEachRemaining(element -> collector.accumulator().accept(accumulator, transform.apply(element)));
         return collector.finisher().apply(accumulator);
     }
 
