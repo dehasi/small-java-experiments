d61b7b430fc09b1cedbd4f271bf70734b53e1fee
Ravil Galeyev <dehasi@yandex.ru>
2021-02-16 23:08:53 +0100

    Add collector

--- src/main/java/mystream/Potok.java
+++ src/main/java/mystream/Potok.java
@@ -4,16 +4,11 @@ import java.util.Spliterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collector;
-import java.util.stream.Stream;
 
 class Potok<IN, OUT> {
 
     private final Spliterator<IN> spliterator;
     private final Function<IN, OUT> transform;
-
-//    private Potok(Spliterator<IN> spliterator) {
-//        this(spliterator, x -> x);
-//    }
 
     private Potok(Spliterator<IN> spliterator, Function<IN, OUT> transform) {
         this.spliterator = spliterator;
@@ -22,15 +17,13 @@ class Potok<IN, OUT> {
 
     @SafeVarargs
     static <TYPE> Potok<TYPE, TYPE> of(TYPE... elements) {
-        return new Potok<>(new MyArraySpliterator<>(elements),  x -> x);
+        return new Potok<>(new MyArraySpliterator<>(elements), x -> x);
     }
 
-
-//    <RESULT> Stream<RESULT> map(Function<? super IN, ? extends RESULT> mapper) {
-//
-//        return new Potok<IN, RESULT>(spliterator, transform.andThen(mapper););
-//    }
-
+    <RES> Potok<IN, RES> map(Function<OUT, RES> mapper) {
+        Function<IN, RES> result = transform.andThen(mapper);
+        return new Potok<>(spliterator, result);
+    }
 
     void forEach(Consumer<? super IN> consumer) {
         while (spliterator.tryAdvance(consumer)) ;
@@ -43,7 +36,9 @@ class Potok<IN, OUT> {
     }
 
     public static void main(String[] args) {
-        String collect = Potok.of(1, 2, 3, 4).collect(AndCollector.joinAnd());
+        String collect = Potok.of(1, 2, 3, 4)
+                              .map(x -> x * x)
+                              .collect(AndCollector.joinAnd());
         System.out.println(collect);
     }
 }
